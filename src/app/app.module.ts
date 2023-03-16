55fdbc79f71b11288f88180d1fdc2091779bbc29
hugo <hugo.herrador@gmail.com>
2023-03-16 17:34:39 +0100

    Remove wrong importation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarModule } from "./shared";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { LogoInmotionComponent } from "./shared/components/logo-inmotion/logo-inmotion.component";
-//import { environment } from 'src/environments/environment';
-//import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 @NgModule({
-	declarations: [AppComponent, LogoInmotionComponent],
+	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, NavbarModule, NgbModule],
 	providers: [],
 	bootstrap: [AppComponent],
