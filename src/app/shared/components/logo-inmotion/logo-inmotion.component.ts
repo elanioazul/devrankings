0d6fc6018a3716ddb576829ac1472258301a9882
hugo <hugo.herrador@gmail.com>
2023-03-16 17:25:37 +0100

    Copy logo-inmotion, not adapted

---
+++ src/app/shared/components/logo-inmotion/logo-inmotion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-logo-inmotion",
+	templateUrl: "./logo-inmotion.component.html",
+	styleUrls: ["./logo-inmotion.component.scss"],
+})
+export class LogoInmotionComponent {}
