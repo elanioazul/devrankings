3eed20e6d5f6714ee28861ea68c9a12bcc2e60ca
hugo <hugo.herrador@gmail.com>
2023-04-16 12:50:04 +0200

    Background photo remain

--- src/app/features/home/scrollslider/scrollslider.component.ts
+++ src/app/features/home/scrollslider/scrollslider.component.ts
@@ -282,7 +282,7 @@ export class ScrollsliderComponent implements OnInit, AfterViewInit {
 				} //ocultar el + si no hay enlace
 				this.slides.forEach((element: any, index) => {
 					if (index !== this.CurrSl) {
-						element.style.setProperty("width", "0%");
+						element.children[0].style.setProperty("width", "0%");
 					}
 				});
 				this.AnimInProgress = false;
