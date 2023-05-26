fea7a37a9a7b960d3f731cda2a19cc7026c26ee3
hugo <hugo.herrador@gmail.com>
2023-05-26 12:31:29 +0200

    Refactor so now event affect particles

--- src/app/features/home/components/interactive-balls/interactive-balls.component.ts
+++ src/app/features/home/components/interactive-balls/interactive-balls.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import * as d3 from "d3";
 @Component({
 	selector: "app-interactive-balls",
@@ -10,39 +10,56 @@ export class InteractiveBallsComponent implements OnInit {
 	height = 500;
 	numNodes = 200;
 	nodes: any;
-	root: any;
-	force: any;
+	nodeElements: any;
 	simulation!: d3.Simulation<any, any>;
 	svg!: d3.Selection<any, any, any, any>;
 	quadTree!: d3.Quadtree<any>;
 
+	@ViewChild("chartContainer", { static: true }) chartContainer!: ElementRef;
+
 	constructor() {}
 
 	ngOnInit() {
-		this.mountNodes();
-
-		this.svgSetUp();
-
+		this.createParticles();
+		this.setSvg();
 		this.setSimulation();
 	}
 
-	mountNodes() {
+	createParticles() {
 		this.nodes = d3.range(this.numNodes).map(() => {
-			return { radius: Math.random() * 15 + 4 };
+			return {
+				radius: Math.random() * 15 + 4,
+				x: Math.random() * this.width,
+				y: Math.random() * this.height,
+			};
 		});
-		this.root = this.nodes[0];
-		this.root.radius = 0;
-		this.root.radius.toFixed();
+	}
+
+	setSvg() {
+		this.svg = d3
+			.select(this.chartContainer.nativeElement)
+			.append("svg")
+			.attr("width", this.width)
+			.attr("height", this.height)
+			.on("mousemove", (e) => {
+				this.simulation.restart();
+			});
+
+		this.nodeElements = this.svg
+			.selectAll("circle")
+			.data(this.nodes.slice(1))
+			.enter()
+			.append("circle")
+			.attr("class", "particle")
+			.attr("r", (d: any) => {
+				return d.radius;
+			})
+			.style("fill", "#e3ff37");
 	}
 
 	setSimulation() {
 		this.simulation = d3
-			.forceSimulation()
-			.nodes(this.nodes)
-			.force("charge", (i) => {
-				return i ? 0 : -2000;
-			})
-			.force("center", d3.forceCenter(this.width / 2, this.height / 2))
+			.forceSimulation(this.nodes)
 			.force(
 				"collide",
 				d3
@@ -52,70 +69,43 @@ export class InteractiveBallsComponent implements OnInit {
 						return d.radius;
 					})
 			)
-			.on("tick", () => this.tick());
-	}
-
-	tick() {
-		this.quadTree = d3.quadtree(this.nodes);
-		let i = 0;
-		let n = this.nodes.length;
-		while (++i < n) this.quadTree.visit(this.collide(this.nodes[i]));
-
-		this.svg
-			.selectAll("circle")
-			.attr("cx", (d: any) => {
-				return d.x;
-			})
-			.attr("cy", (d: any) => {
-				return d.y;
-			});
-	}
-
-	svgSetUp() {
-		this.svg = d3
-			.select(".content")
-			.append("svg")
-			.attr("width", this.width)
-			.attr("height", this.height)
-			.on("mousemove", (e) => {
-				let coords = d3.pointer(e);
-				this.root.px = coords[0];
-				this.root.py = coords[1];
-				this.simulation.restart();
+			.on("tick", () => {
+				this.nodeElements
+					.attr("cx", (d: any) => d.x)
+					.attr("cy", (d: any) => d.y);
 			});
 
-		this.svg
-			.selectAll("circle")
-			.data(this.nodes.slice(1))
-			.enter()
-			.append("circle")
-			.attr("r", (d: any) => {
-				return d.radius;
-			})
-			.style("fill", "#e3ff37");
+		d3.select(this.chartContainer.nativeElement).on("mousemove", (e) => {
+			const [mouseX, mouseY] = d3.pointer(e);
+			this.simulation.force("attract", () => {
+				this.nodes.forEach((particle: any) => {
+					const dx = mouseX - particle.x;
+					const dy = mouseY - particle.y;
+					const distance = Math.sqrt(dx * dx + dy * dy);
+					const minDistance = 100;
+
+					if (distance < minDistance) {
+						const angle = Math.atan2(dy, dx);
+						const force = (minDistance - distance) / minDistance;
+
+						particle.vx += Math.cos(angle) * force;
+						particle.vy += Math.sin(angle) * force;
+					}
+				});
+			});
+		});
 	}
 
-	collide(node: any) {
-		let r = node.radius + 16;
-		let nx1 = node.x - r;
-		let nx2 = node.x + r;
-		let ny1 = node.y - r;
-		let ny2 = node.y + r;
-		return function (quad: any, x1: any, y1: any, x2: any, y2: any) {
-			if (quad.point && quad.point !== node) {
-				let x = node.x - quad.point.x;
-				let y = node.y - quad.point.y;
-				let l = Math.sqrt(x * x + y * y);
-				let r = node.radius + quad.point.radius;
-				if (l < r) {
-					l = ((l - r) / l) * 0.5;
-					node.x -= x *= l;
-					node.y -= y *= l;
-					quad.point.x += x;
-					quad.point.y += y;
-				}
-			}
-			return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-		};
+	runSimulation() {
+		const particles = d3
+			.select(this.chartContainer.nativeElement)
+			.selectAll(".particle");
+		function update() {
+			particles.attr("cx", (d: any) => d.x).attr("cy", (d: any) => d.y);
+
+			requestAnimationFrame(update);
+		}
+
+		update();
 	}
 }
