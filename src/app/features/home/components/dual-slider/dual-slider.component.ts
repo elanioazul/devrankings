ff7a472e74148ce7c02df093869576308b4406c6
hugo <hugo.herrador@gmail.com>
2023-06-11 14:14:01 +0200

    Improve landing page grid style&distribution

---
+++ src/app/features/home/components/dual-slider/dual-slider.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-dual-slider",
+	templateUrl: "./dual-slider.component.html",
+	styleUrls: ["./dual-slider.component.scss"],
+})
+export class DualSliderComponent {}
