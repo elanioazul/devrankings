5852799aa934e088332fa9113c725e18533bc9c7
hugo <hugo.herrador@gmail.com>
2023-07-09 19:04:17 +0200

    create a reusable form validator

--- src/app/features/home/components/contacta/contacta.component.ts
+++ src/app/features/home/components/contacta/contacta.component.ts
@@ -1,8 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
 	selector: "app-contacta",
 	templateUrl: "./contacta.component.html",
 	styleUrls: ["./contacta.component.scss"],
 })
-export class ContactaComponent {}
+export class ContactaComponent {
+	contactaForm!: FormGroup;
+	fb: FormBuilder = inject(FormBuilder);
+
+	loading: boolean = false;
+
+	constructor() {
+		this.contactaForm = this.fb.group({
+			names: ["", Validators.required, Validators.required],
+			email: ["", [Validators.required, Validators.email]],
+			phone: ["", [Validators.required, Validators.minLength(9)]],
+			subject: ["", Validators.required, Validators.required],
+			message: ["", [Validators.required, Validators.minLength(8)]],
+		});
+	}
+
+	load() {
+		this.loading = true;
+
+		setTimeout(() => {
+			this.loading = false;
+		}, 2000);
+	}
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -4,3 +4,4 @@ export * from "./navbar";
 export * from "./logo";
 export * from "./logo-inmotion";
 export * from "./footer";
+export * from "./error-form";
