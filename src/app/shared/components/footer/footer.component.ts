67910597079156243474e5bac9a4d5d41cc0a359
hugo <hugo.herrador@gmail.com>
2024-02-03 13:54:09 +0100

    improve footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,20 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import {
 	faInstagram,
 	faFacebook,
 	faTwitter,
 	faTelegram,
 } from "@fortawesome/free-brands-svg-icons";
+import { selectFeatureCount } from "@core/store/device-size/device-size.selector";
+import { DeviceSizeState } from "@core/store/device-size";
+import { Store, select } from "@ngrx/store";
+import { Subject, Subscription, takeUntil } from "rxjs";
+
 @Component({
 	selector: "app-footer",
 	templateUrl: "./footer.component.html",
 	styleUrls: ["./footer.component.scss"],
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
 	instagram = faInstagram;
 	facebook = faFacebook;
 	twitter = faTwitter;
 	telegram = faTelegram;
 
 	socialMedia = [this.instagram, this.facebook, this.twitter, this.telegram];
+
+	deviceSize: any;
+	public ngDestroyed$ = new Subject();
+	subscription!: Subscription;
+
+	constructor(
+		private store: Store /*private appStore: Store<DeviceSizeState>*/
+	) {
+		this.subscription = this.store
+			.pipe(takeUntil(this.ngDestroyed$))
+			.pipe(select(selectFeatureCount))
+			.subscribe((data: any) => {
+				if (data) {
+					this.deviceSize = data.deviceSize;
+				}
+			});
+	}
+	ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
 }
--- src/app/shared/components/footer/footer.module.ts
+++ src/app/shared/components/footer/footer.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from "@angular/common";
 import { FooterComponent } from "./footer.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { LogoInmotionModule } from "./../logo-inmotion/logo-inmotion.module";
-
+import { LogoModule } from "../logo";
 @NgModule({
 	declarations: [FooterComponent],
-	imports: [CommonModule, FontAwesomeModule, LogoInmotionModule],
+	imports: [CommonModule, FontAwesomeModule, LogoInmotionModule, LogoModule],
 	exports: [FooterComponent],
 })
 export class FooterModule {}
