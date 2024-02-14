e2b1fee9f1b220840735660c0c135ee799a89298
hugo <hugo.herrador@gmail.com>
2024-02-15 00:07:16 +0100

    add custom text to keypoints

--- src/app/features/home/constants/keypoints.ts
+++ src/app/features/home/constants/keypoints.ts
@@ -3,32 +3,32 @@ import { IKeyPoint } from "../interfaces/IkeyPoint";
 export const keyPointsOfDevRankings: IKeyPoint[] = [
 	{
 		icon: "american-sign-language-interpreting",
-		title: "Libero qui dicta impedit? Excepturi?",
-		desc: "Beatae, saepe corrupti ad perferendis aliquid maiores eveniet repudiandae ratione? Maxime dicta est consequatur animi adipisci. Obcaecati, vel!",
+		title: "Experiencia probada gestionando y manteniendo ránkings.",
+		desc: "Clasificaciones precisas para una competencia justa y clara. Precisión basada en datos en la que puede confiar. Nos avalan años de gestion en el ranking de Casa de Campo de Madrid, entre otros.",
 	},
 	{
 		icon: "anchor",
-		title: "Dolores, ullam autem. Sit, repellendus!",
-		desc: "Magnam, commodi expedita? Officiis facere, nemo quod enim quisquam obcaecati beatae praesentium ut ab consequuntur exercitationem molestiae commodi?",
+		title: "Integración de tecnología innovadora.",
+		desc: "Algoritmos de última generación para rankings dinámicos. Soluciones impulsadas por la tecnología para una comunidad deportiva moderna y conectada. Aprovechamiento del poder de la tecnología para una gestión perfecta. Herramientas innovadoras para una administración eficiente del ranking.",
 	},
 	{
 		icon: "coins",
-		title: "Epaa que pasa aquuiii ",
-		desc: "Beatae, saepe corrupti ad perferendis aliquid maiores eveniet repudiandae ratione? Maxime dicta est consequatur animi adipisci. Obcaecati, vel!",
+		title: "Equipo de soporte receptivo y disponible 24/7.",
+		desc: "Apoyo dedicado a jugadores y organizadores. Equipo receptivo para atender consultas e inquietudes. Su éxito es nuestra prioridad: conozca al equipo detrás de las clasificaciones.",
 	},
 	{
 		icon: "hammer",
-		title: "Adios aqui algo nuevo",
-		desc: "Beatae, saepe corrupti ad perferendis aliquid maiores eveniet repudiandae ratione? Maxime dicta est consequatur animi adipisci. Obcaecati, vel!",
+		title: "Soluciones personalizadas.",
+		desc: "Soluciones de ranking personalizadas para cada evento, partiendo de unas bases iniciales para cada deporte. Funciones personalizables para satisfacer las necesidades de tu torneo. Tus rankings, a tu manera: gestión personalizada en su máxima expresión.",
 	},
 	{
 		icon: "sun",
-		title: "Noche de los libros qué dise tu",
-		desc: "Beatae, saepe corrupti ad perferendis aliquid maiores eveniet repudiandae ratione? Maxime dicta est consequatur animi adipisci. Obcaecati, vel!",
+		title: "Análisis integral de datos.",
+		desc: "Análisis de datos en profundidad para obtener conocimientos estratégicos. Desbloquee patrones y tendencias con nuestro análisis integral y visualizaciones. Transforme los datos sin procesar en inteligencia procesable.",
 	},
 	{
 		icon: "microscope",
-		title: "Pla pla Albert pla",
-		desc: "Beatae, saepe corrupti ad perferendis aliquid maiores eveniet repudiandae ratione? Maxime dicta est consequatur animi adipisci. Obcaecati, vel!",
+		title: "Planificación estratégica.",
+		desc: "Planificación estratégica para el éxito del torneo. Optimice la participación de los jugadores con nuestra experiencia en planificación de eventos. Mejora tus torneos con los conocimientos estratégicos de nuestro equipo.",
 	},
 ];
