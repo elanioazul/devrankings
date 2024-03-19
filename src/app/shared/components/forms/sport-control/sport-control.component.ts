93dd1d1dd8d3e4375b458aa022ec48af1c2292d4
hugo <hugo.herrador@gmail.com>
2024-03-19 20:36:24 +0100

    adjust active classes in sport-control

--- src/app/shared/components/forms/sport-control/sport-control.component.ts
+++ src/app/shared/components/forms/sport-control/sport-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { Sport } from "../../../types/sport.types";
 
@@ -15,13 +15,45 @@ import { Sport } from "../../../types/sport.types";
 	],
 })
 export class SportControlComponent implements ControlValueAccessor {
+	@ViewChild("tennisButton") tennisButton!: ElementRef;
+	@ViewChild("squashButton") squashButton!: ElementRef;
+	@ViewChild("golfButton") golfButton!: ElementRef;
+	@ViewChild("tennisFrame") tennisFrame!: ElementRef;
+	@ViewChild("squashFrame") squashFrame!: ElementRef;
+	@ViewChild("golfFrame") golfFrame!: ElementRef;
+
 	sport = "tenis";
 
 	onChange = (value: Sport) => {};
 	onTouch = () => {};
 
+	constructor(private renderer: Renderer2) {}
+
 	setSport(sport: Sport, event: MouseEvent) {
 		event.preventDefault();
+
+		this.renderer.removeClass(this.tennisButton.nativeElement, "active");
+		this.renderer.removeClass(this.squashButton.nativeElement, "active");
+		this.renderer.removeClass(this.golfButton.nativeElement, "active");
+
+		this.renderer.removeClass(this.tennisFrame.nativeElement, "active");
+		this.renderer.removeClass(this.squashFrame.nativeElement, "active");
+		this.renderer.removeClass(this.golfFrame.nativeElement, "active");
+
+		switch (sport) {
+			case "tennis":
+				this.renderer.addClass(this.tennisButton.nativeElement, "active");
+				this.renderer.addClass(this.tennisFrame.nativeElement, "active");
+				break;
+			case "squash":
+				this.renderer.addClass(this.squashButton.nativeElement, "active");
+				this.renderer.addClass(this.squashFrame.nativeElement, "active");
+				break;
+			case "golf":
+				this.renderer.addClass(this.golfButton.nativeElement, "active");
+				this.renderer.addClass(this.golfFrame.nativeElement, "active");
+		}
+
 		this.sport = sport;
 		this.onChange(sport);
 		this.onTouch();
